4078c47c0f222c96e0742033940cba86eb328017
Rahi Khan <rahikhan360@gmail.com>
2020-11-20 10:57:16 +0600

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import Navvy from "./components/Nav/Navvy";
 import TempChart from "./components/Chart/TempChart";
 import ChartButtons from "./components/ChartButtons/ChartButtons";
@@ -7,9 +8,11 @@ const App = () => (
     <>
         <Navvy />
 
-        <div className="main bg-secondary">
-            <TempChart dataset={DATA} />
-            <ChartButtons />
+        <div className="main bg-dark">
+            <Container className="main-section p-0 d-flex justify-content-between align-items-center">
+                <TempChart dataset={DATA} />
+                <ChartButtons />
+            </Container>
         </div>
     </>
 );
--- src/components/ChartButtons/ChartButtons.jsx
+++ src/components/ChartButtons/ChartButtons.jsx
@@ -1,9 +1,23 @@
-const ChartButtons = () => {
-    return (
-        <div>
-            <h1>ChartButtons</h1>
-        </div>
-    );
-};
+import { Button } from "react-bootstrap";
 
+const chartTypes = [
+    "Bar",
+    "Line",
+    "Radar",
+    "Pie",
+    "Polar Area",
+    "Bubble",
+    "Scatter",
+    "Area",
+];
+
+const ChartButtons = () => (
+    <div className="chart-buttons d-flex flex-col">
+        {chartTypes.map(chart => (
+            <Button variant="outline-info" className="mb-3">
+                {chart + " Chart"}
+            </Button>
+        ))}
+    </div>
+);
 export default ChartButtons;
